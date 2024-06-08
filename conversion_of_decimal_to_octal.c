72878941e968b2f29fdecfd390f97be17fcf1768
Neerajkumar199 <145154329+Neerajkumar199@users.noreply.github.com>
2024-06-08 13:32:54 +0530

    Add files via upload

---
+++ conversion_of_decimal_to_octal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+       int x;
+       printf("Enter a number to convert decimal to octal: \n");
+       scanf("%d",&x);
+       printf("Decimal %d - Octal %o:",x,x);
+       return 0 ;
+
+
+}
